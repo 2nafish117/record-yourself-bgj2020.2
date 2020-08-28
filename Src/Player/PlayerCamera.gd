1e608608c3c596654d21281dd5bd4842974cbd1b
2nafish117 <shashank.shashank.c@gmail.com>
2020-08-29 00:47:37 +0530

    camera

--- Src/Player/PlayerCamera.gd
+++ Src/Player/PlayerCamera.gd
@@ -4,5 +4,6 @@ onready var player := get_parent() as RigidBody2D
 export var look_ahead_factor := Vector2(0.3, 0.1)
 export(float) var snappiness := 2.0
 
+
 func _process(delta: float) -> void:
 	position = position.linear_interpolate(player.linear_velocity * look_ahead_factor, delta * snappiness)
